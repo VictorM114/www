c908a681e03197c184e60c1f14a0a1f378e4a026
VictorM114 <victor.ayala1@upr.edu>
2020-02-26 08:31:50 -0400

    Bold en text .php
    bold B

--- test.php
+++ test.php
@@ -6,12 +6,12 @@
 echo "My first PHP script!<br>";#Podemos usar el <br> de HTML como endline
 ?>
 <?php
-$txt = "W3Schools.com";
+$txt = "<b>W3Schools.com</b>";
 echo "I love $txt!";
 
 $num1 = 26;
 $num2 = 4;
-
+echo "<br>";
 echo $num1 + $num2;
 ?>
 </body>
