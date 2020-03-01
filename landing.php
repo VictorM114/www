0513f0427855feba68fa2218ce739543d78122ae
VictorM114 <victor.ayala1@upr.edu>
2020-03-01 16:51:57 -0400

    Create landing.php
    Hice esto pa probar

---
+++ landing.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    echo "Hola! <br><b>Este es mi Landing page!</b>";
+    #Hice este landing page para probar
+
+    ?>
+</body>
+</html>
