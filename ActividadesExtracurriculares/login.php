f1aa439d4d8325d1ff4570cdf9f79b62710dd670
VictorM114 <victor.ayala1@upr.edu>
2020-03-26 15:34:35 -0400

    Cambios en login y css

--- ActividadesExtracurriculares/login.php
+++ ActividadesExtracurriculares/login.php
@@ -8,6 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="over.css">
     <title>Iniciar Sesión</title>
   </head>
   <body>
@@ -17,8 +18,19 @@
       Plataforma Actividades Extracurriculares
       </a> 
   </nav>
-
-        <!-- Footer, alineado en el centro-->
+      <form>
+      <div class="form-group">
+        <label for="email">E-mail</label>
+        <input type="email" class="form-control" placeholder="Ingresar email" id="email">
+      </div>
+      <div class="form-group">
+        <label for="pwd">Password</label>
+        <input type="password" class="form-control" placeholder="Ingresa password" id="pwd">
+        <button type="submit" class="btn btn-primary">Ingresar</button>
+      </div>
+     
+    </form>
+    <!-- Footer, alineado en el centro-->
     <div class="footer" align="center"> 
       <p>&copy Actividades Extracurriculares UPRP, 2020 &copy</p>
     </div>
