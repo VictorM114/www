47404f9aa4aa988dc1595e186737662459e8dc85
VictorM114 <victor.ayala1@upr.edu>
2020-03-26 11:40:26 -0400

    Update login.php
    intento cambio log in

--- ActividadesExtracurriculares/login.php
+++ ActividadesExtracurriculares/login.php
@@ -46,8 +46,8 @@
   <body class="text-center">
     <form class="form-signin">
   <img class="mb-4" src="/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-  <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-  <label for="inputEmail" class="sr-only">Email</label>
+  <h1 class="h3 mb-3 font-weight-normal">Inicar Sesión</h1>
+  <label for="inputEmail" class="sr-only">E-mail</label>
   <input type="email" id="inputEmail" class="form-control" placeholder="Email " required autofocus>
   <label for="inputPassword" class="sr-only">Password</label>
   <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
@@ -57,7 +57,10 @@
     </label>
   </div>
   <button class="btn btn-lg btn-primary btn-block" type="submit">Iniciar sesión</button>
-  <p class="mt-5 mb-3 text-muted">&copy; 2020</p>
+ 
 </form>
 </body>
+<div class="footer" align="center"> 
+  <p>&copy Actividades Extracurriculares UPRP, 2020 &copy</p>
+</div>
 </html>
