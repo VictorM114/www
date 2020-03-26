2e8fe8aa059649ecce3dbaa457a2565e0621b6d0
VictorM114 <victor.ayala1@upr.edu>
2020-03-26 14:16:20 -0400

    Intento login

--- ActividadesExtracurriculares/login.php
+++ ActividadesExtracurriculares/login.php
@@ -8,7 +8,6 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
     <title>Iniciar Sesión</title>
   </head>
   <body>
@@ -18,7 +17,7 @@
       Plataforma Actividades Extracurriculares
       </a> 
   </nav>
-        
+
         <!-- Footer, alineado en el centro-->
     <div class="footer" align="center"> 
       <p>&copy Actividades Extracurriculares UPRP, 2020 &copy</p>
