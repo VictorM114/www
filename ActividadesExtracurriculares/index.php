476410d70c0865c91a2976f11efdd64711a23109
AliceRomero <alicemarieromero@gmail.com>
2020-03-26 15:42:57 -0400

    Update index.php
    cambié el slideshow al que tiene fade en la transición

--- ActividadesExtracurriculares/index.php
+++ ActividadesExtracurriculares/index.php
@@ -25,7 +25,7 @@
 <!--Carousel para las imágenes en la pantalla de inicio, serán fotos de las actividades que hagan las asociaciones
     Se utilizó el framework bootstrap-->
   <!--Un carousel es básicamente un slideshow-->
-  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <!--<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" align = "center">
   <ol class="carousel-indicators">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
@@ -58,7 +58,37 @@
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Próxima</span>
   </a>
+</div>-->
+
+<!--Slideshow con fade-->
+<div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/Asociaciones/IMG_0034.JPG" class="d-block w-100" alt="imagen1">
+    </div>
+    <div class="carousel-item">
+      <img src="images/Asociaciones/IMG_0042.JPG" class="d-block w-100" alt="imagen2">
+    </div>
+    <div class="carousel-item">
+      <img src="images/Asociaciones/IMG_0053.JPG" class="d-block w-100" alt="imagen3">
+    </div>
+    <div class="carousel-item">
+      <img src="images/Asociaciones/IMG_0135.JPG" class="d-block w-100" alt="imagen4">
+    </div>
+    <div class="carousel-item">
+      <img src="images/Asociaciones/IMG_0283.JPG" class="d-block w-100" alt="imagen5">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Anterior</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Siguiente</span>
+  </a>
 </div>
+
 
 <!-- Footer, alineado en el centro-->
 <div class="footer" align="center"> 
