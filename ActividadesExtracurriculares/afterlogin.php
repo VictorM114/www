769f4145e8505f2ca54e098620314e2dfac22392
VictorM114 <victor.ayala1@upr.edu>
2020-03-30 14:53:54 -0400

    after login

--- ActividadesExtracurriculares/afterlogin.php
+++ ActividadesExtracurriculares/afterlogin.php
@@ -9,27 +9,15 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="over.css">
-    <title>Iniciar Sesión</title>
+    <title>Profile</title>
   </head>
   <body>
   <nav class="navbar navbar-light bg-light">
     	  <a class="navbar-brand" href="#">
       <img src="images/logo.jfif" width="30" height="30" class="d-inline-block align-top" alt="">
-      Plataforma Actividades Extracurriculares
       </a> 
   </nav>
-      <form>
-      <div class="form-group">
-        <h3>Bienvenido/Bienvenida!</h3>
-        <label for="usr"><b>Username:</b></label>
-        <input type="usr" class="form-control" placeholder="Ingresar usuario" id="usr">
-        <label for="pwd"><b>Password:</b></label>
-        <input type="password" class="form-control" placeholder="Ingresar password" id="pwd">
-      </div>
-        <div id="btnmedio">
-        <a href = "afterlogin.php"><button type="submit" class="btn btn-primary">Ingresar</button></a>
-        </div>
-    </form>
+     
     <!-- Footer, alineado en el centro-->
     <!--Cambie el tag div por footer-->
     <footer class="footer"> 
