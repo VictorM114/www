c4670830caff28d4aa032809417005755e4ae494
VictorM114 <VictorMAyala11@gmail.com>
2020-04-09 21:50:34 -0400

    hice logout

--- ActividadesExtracurriculares/profile.php
+++ ActividadesExtracurriculares/profile.php
@@ -8,7 +8,6 @@ include 'sconn.php';
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
@@ -48,11 +47,11 @@ include 'sconn.php';
             <a class="nav-link" href="#">Acreditación</a>
           </li>
          </ul>
+         <a href="index.php"><button action = "logout.php" type="button" class="btn btn-primary btn-sm">Log Out</button></a> 
   </nav>
           <div class = "container">
           <h2>Solicitudes Pendientes</h2>
           <p>La asociación tiene número de solicitudes pendientes</p>
-          
           </div>
           <div class = "container">
           <h2>Solicitudes Aprobadas</h2>
@@ -61,19 +60,7 @@ include 'sconn.php';
           </div>
           <div class="container">
             <h2>Actividades Próximas</h2>
-            <p>Estas son las actividades próximas de <?php echo $_SESSION['Fname'];?></p>            
-            <small>*Las actividades mostradas son de las próximas dos semanas*</small>
-            <table class="table table-bordered">
-              <thead>
-                <tr>
-                  <th>Actividad</th>
-                  <th>Lugar</th>
-                  <th>Descripcion</th>
-                  <th>Prop</th>
-                  <th>Horario</th>
-                </tr>
-              </thead>
-              <tbody>
+            <br>
               <?php 
                     /*Selecciona todo de actividades, une asociaciones donde el association ID de 
                     ambas tablas sea igual y me muestras todo donde el nombre de la asociacion sea igual que 
@@ -84,6 +71,20 @@ include 'sconn.php';
                     $result = $conn->query($sql);
                     if ($result->num_rows > 0) {
                       #output de las actividades en una tabla
+                      echo " <p>Estas son las actividades próximas de"; 
+                      echo " " . $_SESSION['Fname'] . ".</p>";            
+                      echo '<small>*Las actividades mostradas son de las próximas dos semanas*</small>
+                      <table class="table table-bordered">
+                        <thead>
+                          <tr>
+                            <th>Actividad</th>
+                            <th>Lugar</th>
+                            <th>Descripcion</th>
+                            <th>Prop</th>
+                            <th>Horario</th>
+                          </tr>
+                        </thead>
+                        <tbody>';
                       while($row = $result->fetch_assoc()){
                         echo "<tr>";
                         echo "<td>" . $row['actName'] . "</td>";
@@ -100,16 +101,11 @@ include 'sconn.php';
               </tbody>
             </table>
           </div>
-    
-
-
-     
     <!-- Footer, alineado en el centro-->
     <!--Cambie el tag div por footer-->
     <footer class="footer"> 
       <p>&copy Actividades Extracurriculares UPRP, <?php echo date("Y");?> &copy </p>
 </footer>
-
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
