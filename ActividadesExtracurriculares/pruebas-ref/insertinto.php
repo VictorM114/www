e48b0854fe7feb83e68343d37f07061be412c679
AliceRomero <alicemarieromero@gmail.com>
2020-04-10 17:10:35 -0400

    Update insertinto.php

--- ActividadesExtracurriculares/pruebas-ref/insertinto.php
+++ ActividadesExtracurriculares/pruebas-ref/insertinto.php
@@ -1,5 +1,14 @@
 <?php 
     include 'sconn.php';
     session_start();
-    
 ?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
