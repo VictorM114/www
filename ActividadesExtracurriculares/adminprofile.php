501b81611f92ccf13d119b76ebf63ba8e77b31b1
AliceRomero <alicemarieromero@gmail.com>
2020-05-07 12:19:55 -0400

    update adminprofile

--- ActividadesExtracurriculares/adminprofile.php
+++ ActividadesExtracurriculares/adminprofile.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="over.css">
-    <title>Admin Profile</title>
+    <title>Administrador</title>
 </head>
 <!-- El nav se utiliza en todas las pantallas con el logo -->
 <body>
